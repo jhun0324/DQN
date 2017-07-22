ca003443e58a7c088d3c18cd1276f371f4ab5b00
won <jhun0324@berkeley.edu>
2017-07-21 17:12:54 -0700

    convDQN.py

--- Atari/convDQN.py
+++ Atari/convDQN.py
@@ -31,18 +31,18 @@ class ConvDQN :
 			self._X = tf.placeholder(shape = [None, self.input_size, self.input_size, 4], dtype = tf.float32)
 	    
 			# hidden layers
-			h_conv1 = tf.nn.relu(conv2d(self._X, W_conv1, 4) + b_conv1)
+			h_conv1 = tf.sigmoid(conv2d(self._X, W_conv1, 4) + b_conv1)
 			h_pool1 = max_pool_2x2(h_conv1)
 
 
-			h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
+			h_conv2 = tf.sigmoid(conv2d(h_pool1, W_conv2, 2) + b_conv2)
 			h_pool2 = max_pool_2x2(h_conv2)
 
 
-			h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, 1) + b_conv3)
+			h_conv3 = tf.sigmoid(conv2d(h_pool2, W_conv3, 1) + b_conv3)
 
 			h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
-			h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+			h_fc1 = tf.sigmoid(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
 			self._Qpred = tf.matmul(h_fc1, W_fc2)
 
