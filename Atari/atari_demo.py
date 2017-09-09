95574c94ceb065b6f024222012a828cfb5c68ffe
won <jhun0324@berkeley.edu>
2017-09-09 15:28:09 -0700

    just demo

--- Atari/atari_demo.py
+++ Atari/atari_demo.py
@@ -12,7 +12,7 @@ while True :
 	print(action)
 	state, reward, done, info = env.step(action)
 	gray_state = np.asarray(cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)) / 255.
-	(thresh, gray_state) = cv2.threshold(gray_state, 0.40, 1., cv2.THRESH_BINARY)
+	(thresh, gray_state) = cv2.threshold(gray_state, 0.4, 1., cv2.THRESH_BINARY)
 	print(gray_state)
 	gray_state = cv2.resize(gray_state, (80, 105))
 	gray_state = gray_state[17:97, :]
